099dc295f018e39741c1fc2efa7b9f9e0ab233a1
PRATHIBHARAGHU <prathibhar070@gmail.com>
2026-06-25 11:22:31 +0530

    Python Basics

---
+++ test.py
@@ -0,0 +1,39 @@
+# print("Hello world", end=" ")
+# print("Hello world", end=" ")
+
+# print("Hello", "world", sep=" #")
+# print("a">"b")
+
+#y="s"
+#x=input() #string
+#s=int(input()) #integer\
+
+# x="10.5"
+# y=int(float(x)) # float to get to base 10 form
+# print(y)
+
+# print("\test\ntesting")
+# print(r"\test\ntesting") #to get in sameline
+# print("\\test\\ntesting")  
+
+
+# control + ? -------> to comment the lines
+
+# print("""hello
+#       my name is
+#       John
+#       """)  # for multiline statements or commands
+
+x="hello"
+print(x[0:3:2]) # start:end:step
+print(x[::-1]) # reverse string
+
+l=[1,2,3,4,5]
+
+for i in range(1,11):
+    if i%2==0:
+        print(i)
+
+for i in range(0,10,2):
+    print(i)
+
